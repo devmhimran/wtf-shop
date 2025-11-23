c3f7e6b263905bef15e7e0957b6236b490b396ac
Imran <mahmud.bubt.150@gmail.com>
2025-11-24 03:12:31 +0600

    add  loading file again

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,5 +1,6 @@
+import { Loading } from './loading';
 import { AppSidebar } from './app-sidebar';
 import { UserNav } from './user-nav';
 import { PrimaryLogo } from './primary-logo';
 
-export { AppSidebar, UserNav, PrimaryLogo };
+export { AppSidebar, UserNav, Loading, PrimaryLogo };
