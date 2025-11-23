866441c720ff6f9b2281349dc34e170f4cc1575d
Imran <mahmud.bubt.150@gmail.com>
2025-11-24 03:11:05 +0600

    remove loading

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,6 +1,5 @@
-import { Loading } from './loading';
 import { AppSidebar } from './app-sidebar';
 import { UserNav } from './user-nav';
 import { PrimaryLogo } from './primary-logo';
 
-export { AppSidebar, UserNav, Loading, PrimaryLogo };
+export { AppSidebar, UserNav, PrimaryLogo };
