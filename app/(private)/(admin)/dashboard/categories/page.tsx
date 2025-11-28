20f0d662afec0b1c812bb1ee7c285db4aff89983
Imran <mahmud.bubt.150@gmail.com>
2025-11-28 19:20:39 +0600

    remove white space

--- app/(private)/(admin)/dashboard/categories/page.tsx
+++ app/(private)/(admin)/dashboard/categories/page.tsx
@@ -46,7 +46,6 @@ export default function CategoriesPage() {
 
   return (
     <div className='space-y-6 w-full md:w-4/6 lg:w-3/6 mx-auto '>
-      {' '}
       <div className='flex items-center justify-between'>
         <h1 className='text-xl md:text-3xl font-bold'>Categories</h1>
         <Button onClick={() => setAddCategoryOpen(true)}>
