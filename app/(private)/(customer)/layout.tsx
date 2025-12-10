da5b350fd38ee60f67b07989c5c5b3d0cb7967de
Imran <mahmud.bubt.150@gmail.com>
2025-12-10 11:26:22 +0600

    add footer and header on customer layout

--- app/(private)/(customer)/layout.tsx
+++ app/(private)/(customer)/layout.tsx
@@ -1,7 +1,15 @@
+import { PublicFooter, PublicNavbar } from '@/components/shared';
+
 export default function CustomerLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <main className='font-oswald'>
+      <PublicNavbar />
+      <div className='min-h-screen mt-20'>{children}</div>
+      <PublicFooter />
+    </main>
+  );
 }
