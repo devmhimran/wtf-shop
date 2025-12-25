0bd79fc86907ab0a431499b10ed632d93acf29e9
Imran <mahmud.bubt.150@gmail.com>
2025-12-25 20:04:41 +0600

    fix pre-rendering issue-1

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -13,7 +13,7 @@ export default function PublicLayout({
   return (
     <main className='font-oswald'>
       <PublicNavbar />
-      <div className='min-h-screen pb-10'>{children}</div>
+      <div className='min-h-screen pb-10'>children</div>
       <PublicFooter />
       <MobileBottomNav />
       <NextTopLoader
